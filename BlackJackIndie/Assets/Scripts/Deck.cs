13d47a7a6dee3af8477336bfa0c8ab66cbb76aab
Tanner <thebow25@gmail.com>
2022-01-25 01:41:59 -0700

    added finished sprites and did deck code that was a int array

---
+++ BlackJackIndie/Assets/Scripts/Deck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+
+    // https://www.youtube.com/watch?v=vVNIoARxH6M&t=460s this is this guys script for the deck
+	public Sprite[] cardSprites;
+
+	private int[] cardValues = new int[53];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
