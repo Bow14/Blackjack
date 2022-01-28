d93aa9cc3b23e221c9182c8053690e48bfeb46f9
Tanner <thebow25@gmail.com>
2022-01-27 23:39:36 -0700

    Added button script prompts

---
+++ BlackJackIndie/Assets/GameManger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManger : MonoBehaviour
+{
+
+	public Button dealBtn;
+	public Button hitBtn;
+	public Button standBtm;
+	public Button betBtn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
