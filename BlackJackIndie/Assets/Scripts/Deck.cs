db05353f9420f220bb62af9bf1abacd7d1f12624
Tanner <thebow25@gmail.com>
2022-02-01 22:29:46 -0700

    Added Array for card data
    with tutorial in the code ofc for the first project back for some flow gain

--- BlackJackIndie/Assets/Scripts/Deck.cs
+++ BlackJackIndie/Assets/Scripts/Deck.cs
@@ -9,6 +9,7 @@ public class Deck : MonoBehaviour
 	public Sprite[] cardSprites;
 
 	private int[] cardValues = new int[53];
+	private int currentIndex = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -19,10 +20,15 @@ public class Deck : MonoBehaviour
 	void getCardValues ()
 	{
 		int num = 0;
+		//assigns values to cards as well as it being a for loop 
+		//Just trying to get back in the flow of learning c# because of all the python ive been doing
+		
 		for (int i = 0; i < cardSprites.Length; i++)
 		{
 			num = i;
+			// counts up to the amount of cards, 52
 			num %= 13;
+			// Divides x/13 then the remainder is used as the value if it is not over 10
 			if (num > 10 || num == 0)
 			{
 				num = 10;
@@ -30,6 +36,23 @@ public class Deck : MonoBehaviour
 			}
 
 			cardValues[i] = num++;
+		}
+
+		currentIndex = 1;
+	}
+
+	public void cardShuffle()
+	{
+		for (int i = cardSprites.Length - 1; i > 0; --i)
+		{
+			int j = Mathf.FloorToInt(Random.Range(0.0f, 1.0f) * cardSprites.Length - 1) + 1;
+			Sprite face = cardSprites[i];
+			cardSprites[i] = cardSprites[j];
+			cardSprites[j] = face;
+
+			int value = cardValues[i];
+			cardValues[i] = cardValues[j];
+			cardValues[j] = value;
 		}
 	}
 }
