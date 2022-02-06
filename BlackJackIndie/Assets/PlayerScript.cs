9cce9af74f3432cb10fef5c3c2d6f02b440e8c34
Tanner <thebow25@gmail.com>
2022-02-06 04:06:53 -0700

    Script added for the player and dealerNonFinished

---
+++ BlackJackIndie/Assets/PlayerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	// https://www.youtube.com/watch?v=Nf0kUpdWeJs
+	//Script is for player and dealer, This works with all the players and dealers cards
+	//Just this entire series is a tutorial to get back into scripting and being able to
+	//Think on my own and experiment with games for future references
+
+	//Get other scripts
+	public CardScript cardScript;
+	public Deck deckScript;
+	
+	// Total value of player/dealers hand
+	public int handValue = 0;
+	//Betting money
+	private int money = 100;
+
+	// Array of card objects on table
+	public GameObject[] hand;
+	//index of next card to be turned over
+	public int cardIndex = 0;
+	public int aceCount = 0;
+	//Tracking aces for 1 to 11 conversions
+	private List<CardScript> aceList = new List<CardScript>();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
